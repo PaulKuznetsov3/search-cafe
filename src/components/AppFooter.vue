eb3ab9af9629d94f2680729ffc158ce0fcbb164f
Pavel <pavel_iv37@mail.ru>
2023-08-02 10:49:47 +0300

    add final

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,30 +1,30 @@
 <template>
-    <v-footer color="#c27800" class="bg-indigo-lighten-1 text-center d-flex flex-column mt">
-      <div class="text">
-        <strong>Наши соцсети</strong>
-      </div>
-      <div class="pt-0 flex">
-        <a class="margin" href="#">{{ null }}
-          <v-img :src="require('@/static/inst.png')"
+  <v-footer color="#c27800" class="bg-indigo-lighten-1 text-center d-flex flex-column mt">
+    <div class="text">
+      <strong>Наши соцсети</strong>
+    </div>
+    <div class="pt-0 flex">
+      <a class="margin" href="#">{{ null }}
+        <v-img :src="require('@/static/inst.png')"
           alt='' :width="30" :height="30"/>
-        </a>
-        <a class="margin" href="#">{{ null }}
-          <v-img :src="require('@/static/fb.png')"
+      </a>
+      <a class="margin" href="#">{{ null }}
+        <v-img :src="require('@/static/fb.png')"
           alt='' :width="30" :height="30"/>
-        </a>
-        <a class="margin" href="#">{{ null }}
-          <v-img :src="require('@/static/linkedin.png')"
+      </a>
+      <a class="margin" href="#">{{ null }}
+        <v-img :src="require('@/static/linkedin.png')"
           alt='' :width="30" :height="30"/>
-        </a>
-        <a class="margin" href="#">{{ null }}
-          <v-img :src="require('@/static/tw.png')"
+      </a>
+      <a class="margin" href="#">{{ null }}
+        <v-img :src="require('@/static/tw.png')"
           alt='' :width="30" :height="30"/>
-        </a>
-      </div>
-      <v-divider></v-divider>
-      <div>{{ new Date().getFullYear() }} — <strong>Search Cafe</strong></div>
-    </v-footer>
-  </template>
+      </a>
+    </div>
+    <v-divider></v-divider>
+    <div>{{ new Date().getFullYear() }} — <strong>Search Cafe</strong></div>
+  </v-footer>
+</template>
 <script>
 export default {
   data: () => ({
